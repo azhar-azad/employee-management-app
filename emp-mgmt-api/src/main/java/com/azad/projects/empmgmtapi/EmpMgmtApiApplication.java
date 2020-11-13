ddcead83fed5fcfa7c6931095c1c79655c4aef57
azhar-azad <azhar.u.azad@gmail.com>
2020-11-14 00:52:10 +0600

    emp-mgmt-api project initialized

---
+++ emp-mgmt-api/src/main/java/com/azad/projects/empmgmtapi/EmpMgmtApiApplication.java
@@ -0,0 +1,13 @@
+package com.azad.projects.empmgmtapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpMgmtApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmpMgmtApiApplication.class, args);
+    }
+
+}
