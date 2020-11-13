3bd93a32e8ff5411d742d6b04696be8cb440e0ef
azhar-azad <azhar.u.azad@gmail.com>
2020-11-14 01:32:13 +0600

    ResourceNotFoundException added

---
+++ emp-mgmt-api/src/main/java/com/azad/projects/empmgmtapi/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,14 @@
+package com.azad.projects.empmgmtapi.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+
+    private static final long serialVersionId = 1L;
+
+    public ResourceNotFoundException(String s) {
+        super(s);
+    }
+}
