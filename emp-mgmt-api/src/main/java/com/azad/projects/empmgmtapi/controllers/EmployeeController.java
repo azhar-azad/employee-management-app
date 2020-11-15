c4d5f37633b58cfbf42d24933e679361f9a395a8
azhar-azad <azhar.u.azad@gmail.com>
2020-11-15 13:35:03 +0600

    Get Employee by id endpoint implemented

--- emp-mgmt-api/src/main/java/com/azad/projects/empmgmtapi/controllers/EmployeeController.java
+++ emp-mgmt-api/src/main/java/com/azad/projects/empmgmtapi/controllers/EmployeeController.java
@@ -1,8 +1,10 @@
 package com.azad.projects.empmgmtapi.controllers;
 
 import com.azad.projects.empmgmtapi.entities.Employee;
+import com.azad.projects.empmgmtapi.exceptions.ResourceNotFoundException;
 import com.azad.projects.empmgmtapi.repos.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -29,5 +31,14 @@ public class EmployeeController {
     @PostMapping(path = "/employees")
     public Employee createEmployee(@RequestBody Employee employee) {
         return employeeRepository.save(employee);
+    }
+
+    // get employee by id
+    @GetMapping(path = "/employees/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable long id) {
+        Employee employee =  employeeRepository.findById(id).orElseThrow(() ->
+           new ResourceNotFoundException("Employee not found with id: " + id));
+
+        return ResponseEntity.ok(employee);
     }
 }
